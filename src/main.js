64032b1fbc21b27ccd57c199514b5593e5cda1b3
galexbh <galexbh@protonmail.com>
2022-04-10 18:49:37 -0600

    feat: :tada: Start Project

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron');
+const { join } = require('path');
+
+const isDev = !app.isPackaged;
+app.whenReady().then(main);
+
+function main() {
+  const window = new BrowserWindow({
+    with: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    },
+  });
+
+  window.loadFile(join(__dirname, './ui/index.html'));
+  window.on('ready-to-show', window.show);
+
+  if (isDev) window.webContents.openDevTools();
+}
+
+module.exports = {
+    main
+}
